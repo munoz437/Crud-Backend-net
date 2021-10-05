5068b934f2d78cc91748fe89805eb7ccc337d903
Jorge <jorgeelias437@gmail.com>
2021-10-05 13:00:42 -0600

    mas cambios

--- api_empresa/Controllers/EmpleadosController.cs
+++ api_empresa/Controllers/EmpleadosController.cs
@@ -31,7 +31,15 @@ namespace api_empresa.Controllers{
         //Get api/Empleados/1
         [HttpGet("{id}")]
         public async Task<ActionResult> Get(int id) {
-            var empleados = await dbConexion.Empleados.FindAsync(id);
+            // var empleados = await dbConexion.Empleados.FindAsync(id);
+             var empleados =  from a in dbConexion.Empleados
+                        join s in dbConexion.Puestos
+                        on a.id_puesto equals s.id_puesto
+                        where a.id_empleado==id
+                        select new Empleados{ id_empleado=a.id_empleado, codigo=a.codigo,nombres= a.nombres, apellidos = a.apellidos,
+                            direccion = a.direccion, telefono = a.telefono, fecha_nacimiento = a.fecha_nacimiento,id_puesto=a.id_puesto,puesto= s };
+           
+            
             if (empleados != null) {
                 return Ok(empleados);
             } else {
@@ -39,17 +47,7 @@ namespace api_empresa.Controllers{
             }
         }
 
-        //Post para insertar datos
-        //Post api/Empleados
-        // {
-        // "id_empleado": 1,
-        // "codigo": "E001",
-        // "nombres": "Mario Haroldo",
-        // "apellidos": "Sinay Gaytan",
-        // "direccion": "Antigua",
-        // "telefono": "30319066",
-        // "fecha_nacimiento": "11/03/2000 00:00:00",
-        // "id_puesto": 3
+        
         [HttpPost]
         public async Task<ActionResult> Post([FromBody] Empleados empleados) {
             if (ModelState.IsValid){
@@ -61,17 +59,7 @@ namespace api_empresa.Controllers{
             }
         }
 
-        //update
-        //Put api/Empleados/1
-        // {
-        // "id_empleado": 1,
-        // "codigo": "E001",
-        // "nombres": "Mario Haroldo",
-        // "apellidos": "Sinay Gaytan",
-        // "direccion": "Antigua",
-        // "telefono": "30319066",
-        // "fecha_nacimiento": "11/03/2000 00:00:00",
-        // "id_puesto": 3
+        
         public async Task<ActionResult> Put([FromBody] Empleados empleados){
             var v_empleados = dbConexion.Empleados.SingleOrDefault(c => c.id_empleado == empleados.id_empleado);
             if (v_empleados != null && ModelState.IsValid){
@@ -84,9 +72,7 @@ namespace api_empresa.Controllers{
         }
 
 
-        //Get para mostrar un valor
-        //Método asíncrono
-        //Delete api/Empleados/1
+        
         [HttpDelete("{id}")]
         public async Task<ActionResult> Delete(int id) {
             var empleados = dbConexion.Empleados.SingleOrDefault(c => c.id_empleado == id);
